c7425dff426b17d7319f589614b721729ac59348
harsh savani <131761988+harshsavani69@users.noreply.github.com>
2023-05-12 21:34:06 +0530

    Add files via upload

---
+++ TS-1.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   clrscr();
+   char i;
+   printf("the alphabet from a to z are ,\n");
+   i='a';
+   do
+   {
+	printf("%c",i);
+	i++;
+   } while(i<='z');
+   getch();
+}
